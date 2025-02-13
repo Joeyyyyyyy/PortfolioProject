13aed4d4b1e64fff2ed075d532c8b892710d3aa3
Joeyyyyyyy <jjjj8912@gmail.com>
2025-02-13 11:13:52 +0530

    Update StockPortfolio.py

--- StockPortfolio.py
+++ StockPortfolio.py
@@ -454,10 +454,13 @@ class MarketStatus:
 
         # Formatting date
         today_date = datetime.now().strftime('%d/%m/%Y, %I:%M:%S %p')
+        
+        open=self.is_market_open()
+        open_or_close = "open."if open else "closed."
 
         # Generating the result string
         result = (f"SENSEX is {sensex_status} by {abs(sensex_change):.2f} points while NIFTY50 is {nifty_status} "
-                f"by {abs(nifty_change):.2f} points today. Today's date and time: ({today_date})")
+                f"by {abs(nifty_change):.2f} points today. Today's date and time: ({today_date})."+" Market is "+open_or_close)
 
         return result
 
